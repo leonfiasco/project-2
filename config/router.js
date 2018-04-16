e4245688ff671d3939e3c23a058186bb69c64a08
leon Boakye-Adjei <leon_adjei@hotmail.com>
2018-04-16 09:31:22 +0100

    first commit

---
+++ config/router.js
@@ -0,0 +1,17 @@
+//creating an express router at top of file
+const router = require('express').Router();
+const fantasy = require('../data/fantasy');
+const action = require('../data/action');
+
+//adding routes
+router.get('/', (req, res) => res.render('pages/home'));
+router.get('/about', (req, res) => res.render('pages/about'));
+router.get('/contact', (req, res) => res.render('pages/contact'));
+
+router.get('/fantasy', (req, res) => res.render('pages/category', { data: fantasy }));
+router.get('/action', (req, res) => res.render('pages/category', { data: action }));
+
+
+
+
+module.exports = router;
---
+++ index.js
@@ -0,0 +1,32 @@
+// requiring express
+const express = require('express');
+// creating the app
+const app = express();
+//setting up the port
+const PORT = process.env.PORT || 8000;
+//tell express to use express-ejs-layouts
+const expressLayouts = require('express-ejs-layouts');
+//requiring router
+const router = require('./config/router');
+
+
+
+//configuring express to use ejs
+app.set('view engine', 'ejs');
+//looks for files in my views folder
+app.set('views', `${__dirname}/views`);
+//tell express to use express-ejs-layouts
+app.use(expressLayouts);
+
+
+//telling express to look the folder for static files
+app.use(express.static(`${__dirname}/public`));
+
+//adding request listener to serve home template
+app.get('/', (req, res) => res.render('pages/home'));
+
+//tell express to use Router
+app.use(router);
+
+
+app.listen(PORT, () => console.log(`Up and running on port ${PORT}`));
